766112dea5eb4793d096ce1e4328153d3b557732
s-kngstn <s.kngstn@protonmail.com>
2021-05-05 18:52:24 +0100

    Updated functions to arrow functions

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ const btn = document.querySelector("#btn");
 const content = document.querySelector(".content-container");
 new ClipboardJS(".copy");
 
-function shuffleWords(arr) {
+const shuffleWords = (arr) => {
   for (let i = arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [arr[i], arr[j]] = [arr[j], arr[i]];
@@ -48,12 +48,12 @@ function shuffleWords(arr) {
   return arr;
 }
 
-function getWords(words, wordAmount) {
+const getWords = (words, wordAmount) => {
   let removeSpecialChars = /[^\w\s]/gi;
   return words.split(" ").slice(0, wordAmount).join(" ").toLowerCase().replace(removeSpecialChars, '');
 }
 
-function getSentences(amount) {
+const getSentences = (amount) => {
   let newSentenceArr = [];
   for (let i = 0; i < amount; i++) {
     let randomSentence =
@@ -63,7 +63,7 @@ function getSentences(amount) {
   return newSentenceArr.join(" ");
 }
 
-function getParas(amount) {
+const getParas = (amount) => {
   let para = [];
   for (let i = 0; i < amount; i++) {
     // Randomly output 5-10 sentence paragraphs
